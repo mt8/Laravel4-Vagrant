332763c9c9d624fed4d2214acf55d282ccb9c9d5
Hirohisa Kawase <hirokws@gmail.com>
2013-08-10 21:45:57 +0900

    correct issue on install of Laraverl4.0

--- puppet/modules/laravel_app/manifests/init.pp
+++ puppet/modules/laravel_app/manifests/init.pp
@@ -1,4 +1,4 @@
-class laravel_app 
+class laravel_app
 {
 
 	package { 'git-core':
@@ -20,29 +20,35 @@ class laravel_app
 	# Check to see if there's a composer.json and app directory before we delete everything
 	# We need to clean the directory in case a .DS_STORE file or other junk pops up before
 	# the composer create-project is called
-	exec { 'clean www directory': 
+	exec { 'clean www directory':
 		command => "/bin/sh -c 'cd /var/www && find -mindepth 1 -delete'",
 		unless => [ "test -f /var/www/composer.json", "test -d /var/www/app" ]
 	}
 
 
-	exec { 'create laravel project':
-		command => "/bin/sh -c 'cd /var/www/ && composer create-project laravel/laravel . -n --prefer-dist'",
-		require => [Exec['global composer'], Package['git-core'], Exec['clean www directory']],
-		creates => "/var/www/composer.json",
-		timeout => 1800,
-	}
+#    exec { 'create laravel project':
+#        command => "/bin/sh -c 'cd /var/www/ && composer create-project laravel/laravel . -n --prefer-dist'",
+#        require => [Exec['global composer'], Package['git-core'], Exec['clean www directory']],
+#        creates => "/var/www/composer.json",
+#        timeout => 1800,
+#    }
+    exec { 'create laravel project':
+        command => "/bin/sh -c 'cd /var/www/ && git clone https://github.com/HiroKws/laravel.git .'",
+        require => [Package['git-core'], Exec['clean www directory']],
+        creates => "/var/www/composer.json",
+        timeout => 1800,
+    }
 
 	exec { 'update packages':
         command => "/bin/sh -c 'cd /var/www/ && composer update'",
-        require => [Package['git-core'], Package['php5']],
+        require => [Package['git-core'], Package['php5'], Exec['global composer']],
         onlyif => [ "test -f /var/www/composer.json", "test -d /var/www/vendor" ],
         timeout => 900,
 	}
 
 	exec { 'install packages':
         command => "/bin/sh -c 'cd /var/www/ && composer install'",
-        require => Package['git-core'],
+        require => [Package['git-core'],Exec['global composer']],
         onlyif => [ "test -f /var/www/composer.json" ],
         creates => "/var/www/vendor/autoload.php",
         timeout => 900,
